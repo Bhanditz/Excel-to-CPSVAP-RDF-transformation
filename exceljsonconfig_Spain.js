326df73ba83e3eccc9eb60eed6a1f7ac0d28805b
carmenvandeloo <carmen_vandeloo@outlook.com>
2018-08-24 18:46:47 +0200

    Convert Excel to JSON for Spain

--- exceljsonconfig_Spain.js
+++ exceljsonconfig_Spain.js
@@ -7,7 +7,7 @@ const result = excelToJson({
         // Is the number of rows that will be skipped and will not be present at our result object. Counting from top to bottom
         rows: 2
     },
-    sheets: ['ES_PS-en'],
+    sheets: ['ES_PS-en', 'ES_PS-es'],
     columnToKey: {
         B: 'PublicService_id',
         C: 'PublicService_name',
@@ -27,9 +27,19 @@ const result = excelToJson({
 });
 
 const fs = require('fs');
-const content = JSON.stringify(result);
+var content = JSON.stringify(result);
+content = content.replace("ES_PS-en", "English");
+content = content.replace("ES_PS-es", "Spanish");
+content = content.replace(/\\n/g, "\\n")
+                                      .replace(/\\'/g, "\\'")
+                                      .replace(/\\"/g, '\\"')
+                                      .replace(/\\&/g, "\\&")
+                                      .replace(/\\r/g, "\\r")
+                                      .replace(/\\t/g, "\\t")
+                                      .replace(/\\b/g, "\\b")
+                                      .replace(/\\f/g, "\\f");
 
-fs.writeFile("./tmp/test.json", content, 'utf8', function (err) {
+fs.writeFile("./tmp/dataSpain.json", content, 'utf8', function (err) {
     if (err) {
         return console.log(err);
     }
